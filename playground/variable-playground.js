8cba22ebbe1a77d89504551341d008165a2e8812
Antonio Campos <antj.campos@gmail.com>
2015-12-09 01:16:22 +0000

    Initial Commit

---
+++ playground/variable-playground.js
@@ -0,0 +1,30 @@
+/*var person = {
+	name: 'Andrew',
+	age: 21
+};
+
+function updatePerson (obj) {
+	//obj = {
+	//	name: 'Andrew',
+	//	age: 24
+	//};
+	obj.age = 24;
+}
+
+updatePerson(person);
+console.log(person);*/
+
+// Array example
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+	gradesArr.push(55);
+	debugger;
+	//grades = [12, 33, 99];
+}
+
+addGrades(grades);
+console.log(grades);
+
+
+
